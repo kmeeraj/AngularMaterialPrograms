15be86afa15d50dad533f3576319f5ccdaddd734
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-02-04 00:24:42 +0530

    10_SPINNER

--- src/app/services/lessons.datasource.ts
+++ src/app/services/lessons.datasource.ts
@@ -3,11 +3,13 @@ import {Lesson} from '../model/lesson';
 import {CollectionViewer} from '@angular/cdk/collections';
 import {BehaviorSubject, Observable, of} from 'rxjs';
 import {CoursesService} from './courses.service';
-import {catchError} from 'rxjs/operators';
+import {catchError, finalize} from 'rxjs/operators';
 
 export class LessonsDatasource implements DataSource<Lesson> {
 
   private lessonSubject = new BehaviorSubject<Lesson[]>([]);
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  public loading$ = this.loadingSubject.asObservable();
   constructor(private  courseService: CoursesService) {
 
   }
@@ -15,14 +17,17 @@ export class LessonsDatasource implements DataSource<Lesson> {
     return this.lessonSubject.asObservable();
   }
   loadLessons(courseId: number, filter: string, sortDirection: string, pageIndex: number, pageSize: number) {
+    this.loadingSubject.next(true);
     this.courseService.findLessons(courseId, filter, sortDirection, pageIndex, pageSize)
       .pipe(
-        catchError(() => of([]))
+        catchError(() => of([])),
+        finalize(() => this.loadingSubject.next(false))
       )
       .subscribe(lessons => this.lessonSubject.next(lessons));
   }
   disconnect(collectionViewer: CollectionViewer): void {
     this.lessonSubject.complete();
+    this.loadingSubject.complete();
   }
 
 }
