27d2bd0d8ac8027e5ce006c8c5f5c94765fc93a4
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-02-04 00:12:58 +0530

    9_PAGINATION

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {CoursesService} from '../services/courses.service';
 import {ActivatedRoute} from '@angular/router';
 import {Course} from '../model/course';
 import {Observable} from 'rxjs';
-import {MatTableDataSource} from '@angular/material';
+import {MatPaginator, MatTableDataSource} from '@angular/material';
 import {LessonsDatasource} from '../services/lessons.datasource';
+import {startWith, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css']
 })
-export class CourseComponent implements OnInit {
+export class CourseComponent implements OnInit , AfterViewInit {
   course: Course;
   // dataSource = new MatTableDataSource([]);
   dataSource: LessonsDatasource;
   displayedColumns = ['seqNo', 'description', 'duration'];
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
   constructor(private route: ActivatedRoute,
               private coursesService: CoursesService) { }
 
@@ -24,7 +27,21 @@ export class CourseComponent implements OnInit {
     // this.coursesService.findAllCoursesLessons(this.course.id)
     //   .subscribe(lessons => this.dataSource.data = lessons);
     this.dataSource = new LessonsDatasource(this.coursesService);
-    this.dataSource.loadLessons(this.course.id, 'Hello', 'desc', 0, 3);
+    // this.dataSource.loadLessons(this.course.id, '', 'asc', 0, 3);
+  }
+
+  ngAfterViewInit(): void {
+    this.paginator.page
+      .pipe(
+        startWith(null),
+        tap(() => {
+          this.dataSource.loadLessons(this.course.id, '', 'asc', this.paginator.pageIndex, this.paginator.pageSize);
+        })
+      )
+     /* .subscribe(() => {
+        this.dataSource.loadLessons(this.course.id, '', 'asc', this.paginator.pageIndex, this.paginator.pageSize);
+      })*/
+      .subscribe();
   }
 
   /*searchLessons(search: string) {
